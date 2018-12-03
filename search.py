779a169f7decf1e6750f055d131604558b185ccb
Josh Mayers <wwedabest@Josh-2.local>
2018-12-03 14:48:31 -0500

    Updated pySearch, search, & README

--- search.py
+++ search.py
@@ -1,13 +1,23 @@
 
 import urllib
 import webbrowser
+import re
+
+#print available broswers
+def printAvailableBrowsers(invalid):
+        print("You have selected an invalid or unreigstered browser: " + invalid + ".\nHere is a list of available browsers")
+        for i in webbrowser._browsers:
+            print("\t"+i)
+#end of printAvailableBrowsers
+
 
 class Search:
-    def __init__(self, searchIn = None, engineIn = "google", domainIn = "ca"):
+    def __init__(self, searchIn = None, engineIn = "google", domainIn = "ca", browser=None):
         self.searchRaw = searchIn
         self.searchQuery = ""
         self.engine = engineIn
         self.domain = domainIn
+        self.browser = browser
         self.url = ""
         self.searchString = "/search?q="
     #end of constructor
@@ -27,6 +37,27 @@ class Search:
         self.searchRaw = searchIn
     #end of setQuery
 
+    #set browser
+    def setBrowser(self, browser):
+        regex = {'chrome':'(google|chrome|google chrome|google-chrome)', 
+                 'firefox':'(firefox|mozilla|mozilla firefox|mozilla-firefox)',
+                 'iexplore':'(ie|internet explorer|internet-explorer|iexplorer|iexplore)',
+                 'safari':'safari'}
+        for i in regex:
+            match = re.search(regex[i], browser, re.IGNORECASE)
+            if match:
+                browser = i
+        self.browser = browser
+    #end of setBrowser
+
+    #get browser
+    def getBrowser(self):
+        try:
+            webbrowser.get(self.browser).open_new_tab(self.url)
+        except:
+            printAvailableBrowsers(self.browser)
+    #end of get browser
+
     def buildLink(self):
         if self.engine == "amazon":
             self.searchString = "/s/keywords="
@@ -41,7 +72,10 @@ class Search:
     #end of link building
 
     def openBrowser(self):
-        webbrowser.open_new_tab(self.url)
+        if self.browser is None:
+            webbrowser.open_new_tab(self.url)
+        else:
+            self.getBrowser()
     #end of openBrowser()
 
     def handleArgs(self, args):
@@ -50,6 +84,9 @@ class Search:
 
         if args.domain is not None:
             self.setDomain(args.domain[0])
+
+        if args.browser is not None:
+            self.setBrowser(args.browser[0])
 
         for search in args.s:
             self.setQuery(search)
